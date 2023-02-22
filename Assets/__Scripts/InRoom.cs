2231d84b76765346972e6ba2e6eee7f7e98801f8
zyy44030 <36081282+zyy44030@users.noreply.github.com>
2023-02-22 23:30:08 +0800

    keep in room

--- Assets/__Scripts/InRoom.cs
+++ Assets/__Scripts/InRoom.cs
@@ -7,18 +7,34 @@ public class InRoom : MonoBehaviour
     static public float ROOM_W = 16;
     static public float ROOM_H = 11;
     static public float WALL_T = 2;
+    
+    [Header("Set in Inspector")]
+    public bool keepInRoom = true;
+    public float gridMult = 1;
+
+    private void LateUpdate() {
+        if(keepInRoom){
+            Vector2 rPos = roomPos;
+            rPos.x = Mathf.Clamp(rPos.x, WALL_T, ROOM_W - 1 - WALL_T);
+            rPos.y = Mathf.Clamp(rPos.y, WALL_T, ROOM_H - 1 - WALL_T);
+            roomPos = rPos;
+        }
+    }
+
     public Vector2 roomPos{
         get{
             Vector2 tPos = transform.position;
             tPos.x %= ROOM_W;
             tPos.y %= ROOM_H;
             return tPos;
+
         }
         set{
             Vector2 rm = roomNum;
             rm.x *= ROOM_W;
             rm.y *= ROOM_H;
             rm += value;
+            transform.position = rm;
         }
     }
 
@@ -26,7 +42,7 @@ public class InRoom : MonoBehaviour
         get{
             Vector2 tPos = transform.position;
             tPos.x = Mathf.Floor(tPos.x / ROOM_W);
-            tPos.x = Mathf.Floor(tPos.y / ROOM_H);
+            tPos.y = Mathf.Floor(tPos.y / ROOM_H);
             return tPos;
         }
         set{
